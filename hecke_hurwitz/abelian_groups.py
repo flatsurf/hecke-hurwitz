9621cdef951d1991af672d7dd060e41bba0ba14a
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2022-08-09 20:24:19 +0200

    fix imports and doctests

--- hecke_hurwitz/abelian_groups.py
+++ hecke_hurwitz/abelian_groups.py
@@ -1,5 +1,15 @@
+r"""
+Tools for manipulating Abelian groups
+"""
 from sage.misc.misc_c import prod
+from sage.misc.cachefunc import cached_function
+
+from sage.arith.functions import lcm
+
 from sage.groups.perm_gps.permgroup_named import SymmetricGroup
+from sage.modules.free_module import VectorSpace
+from sage.rings.number_field.number_field import CyclotomicField
+
 
 def abelian_group_element_encoding(invariants, g):
     r"""
@@ -62,6 +72,7 @@ def abelian_group_mul_permutation(invariants, a):
 
         sage: from hecke_hurwitz.abelian_groups import abelian_group_mul_permutation
         sage: abelian_group_mul_permutation((10, 2), (-1, 1))
+        (2,10)(3,9)(4,8)(5,7)(12,20)(13,19)(14,18)(15,17)
     """
     n = prod(invariants)
     S = SymmetricGroup(n)
@@ -77,7 +88,8 @@ def abelian_group_characters(invariants):
     r"""
     EXAMPLES::
 
-        sage: ch = matrix(characters((10,2)))
+        sage: from hecke_hurwitz.abelian_groups import abelian_group_characters
+        sage: ch = matrix(abelian_group_characters((10,2)))
         sage: ch.rank()
         20
         sage: matrix([[s.hermitian_inner_product(t).abs() for s in ch.rows()] for t in ch.rows()]) == 20 * identity_matrix(20)
